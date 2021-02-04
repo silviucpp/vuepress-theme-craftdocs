e680ce51161973d5954a3ab78d0aec1fb8664d04
silviu caragea <silviu.cpp@gmail.com>
2021-02-04 14:09:02 +0200

    Style 404 page

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,26 +1,78 @@
 <template>
-  <div class="theme-container">
-    <div class="content__default">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
-    </div>
+  <div class="theme-container" :class="pageClasses">
+    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
+
+      <div class="homepage">
+        <div class="mainbox">
+          <div class="err">404</div>
+          <div class="msg">Maybe this page moved? Got deleted?  Never existed in the first place? <p>Let's go <a href="/">home</a> and try from there.</p></div>
+        </div>
+      </div>  
   </div>
 </template>
 
 <script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
+
+import Navbar from '../components/Navbar.vue'
 
 export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
+  components: {Navbar},
+
+  computed: {
+    shouldShowNavbar () {
+      const { themeConfig } = this.$site
+
+      if (themeConfig.navbar === false)
+        return false
+
+      return (this.$title || themeConfig.logo || themeConfig.repo || themeConfig.nav || this.$themeLocaleConfig.nav)
+    },
+
+    pageClasses () {
+      return [
+        {
+          'no-navbar': !this.shouldShowNavbar
+        }
+      ]
     }
-  }
+  },
 }
 </script>
+
+<style lang="stylus">
+@import '../styles/config.styl'
+
+.homepage {
+  width: 100%;
+  height: 100vh;
+  background-color: $navbarBackgroundColor;
+}
+
+.mainbox {
+  color: #ffffff;
+  margin: auto;
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+
+.err {
+  text-align: center;
+  font-weight: 600;
+  font-size: 11rem;
+  color: $accentColor;
+  position:absolute;
+  top: 8%;
+  width: 100%;
+}
+
+.msg {
+    text-align: center;
+    font-size: 1.6rem;
+    position:absolute;
+    left: 10%;
+    top: 40%;
+    width: 80%;
+  }
+</style>
+
